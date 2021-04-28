2aca83ba2694eebc6e5b8a1119d9c92931d5b495
ajf013 <jeni13franc@gmail.com>
2021-04-28 09:54:39 +0530

    Full code uploaded

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="http://francis-cruz-portfolio.web.app/" target="_blank" rel="noopener noreferrer"
+        style={{ cursor: "pointer" }}
+      >
+        AJF013
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
